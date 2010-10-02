5107b015649de048a7f69355254e3ee18507cb1f
unknown <Toran@.(none)>
2010-10-02 17:06:31 -0500

    Episode 3

--- TddWithFitnesse/Controllers/PostController.cs
+++ TddWithFitnesse/Controllers/PostController.cs
@@ -6,26 +6,24 @@ using System.Web.Mvc;
 using TddWithFitnesse.Models;
 using System.Data.SqlClient;
 using System.Data;
+using TddWithFitnesse.Repository;
 
 namespace TddWithFitnesse.Controllers
 {
     public class PostController : Controller
     {
-        private string connectionString = @"server=core2duo\SQLEXPRESS;DATABASE=dsmtbillup;Trusted_Connection=Yes";
+        private PostRepository repository;
+
+        public PostController() : this(new PostRepository()) { }
+
+        public PostController(PostRepository repository) 
+        {
+            this.repository = repository;
+        }
+
         public ViewResult CreatePost(Post post)
         {
-            var identityValue = new SqlParameter("@PostID", SqlDbType.Int) { Direction = ParameterDirection.Output };
-            using (SqlCommand cmd = new SqlConnection().CreateCommand())
-            {
-                cmd.Parameters.Add(identityValue);
-                cmd.CommandText = "insert into posts (Title, Content, Uri) values ('" + post.Title + "','" + post.Content + "','" + post.Uri + "');SET @PostID = SCOPE_IDENTITY()";
-                cmd.Connection = new SqlConnection(connectionString);
-                cmd.Connection.Open();
-
-                cmd.ExecuteNonQuery();
-
-                post.ID = (int)identityValue.Value;
-            }
+            repository.InsertPost(post);
 
             return View("CreatePost", post);
         }
---
+++ TddWithFitnesse/Repository/PostRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TddWithFitnesse.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+namespace TddWithFitnesse.Repository
+{
+    public class PostRepository
+    {
+        private string connectionString = @"server=core2duo\SQLEXPRESS;DATABASE=dsmtbillup;Trusted_Connection=Yes";
+
+        public void InsertPost(Post post)
+        {
+            var identityValue = BuildIdentityValueParameter();
+            using (SqlCommand cmd = new SqlConnection().CreateCommand())
+            {
+                cmd.Parameters.Add(identityValue);
+                AddParameterValuesForInsert(cmd, post);
+                cmd.CommandText = BuildInsertSql(post);
+                cmd.Connection = new SqlConnection(connectionString);
+                cmd.Connection.Open();
+
+                cmd.ExecuteNonQuery();
+
+                post.ID = (int)identityValue.Value;
+            }
+        }
+
+        private string BuildInsertSql(Post post)
+        {
+            var insertSql = new StringBuilder();
+            insertSql.Append("insert into posts (Title,Content,Uri) values (@title,@content,@uri);");
+            insertSql.Append("SET @PostID = SCOPE_IDENTITY()");
+
+            return insertSql.ToString();
+        }
+
+        private void AddParameterValuesForInsert(SqlCommand cmd, Post post)
+        {
+            cmd.Parameters.Add(new SqlParameter("@title", SqlDbType.VarChar) { Value = post.Title });
+            cmd.Parameters.Add(new SqlParameter("@content", SqlDbType.VarChar) { Value = post.Content });
+            cmd.Parameters.Add(new SqlParameter("@uri", SqlDbType.VarChar) { Value = post.Uri });
+        }
+
+        private SqlParameter BuildIdentityValueParameter()
+        {
+            return new SqlParameter("@PostID", SqlDbType.Int) { Direction = ParameterDirection.Output };
+        }
+    }
+}
